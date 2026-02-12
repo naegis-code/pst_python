1a4b9554c4d630f9aca235bf32ca3aa57ea2727e
prthanapat <thanapatpromchat@gmail.com>
2026-02-12 11:43:59 +0700

    add var_to_db3 function for data insertion with progress tracking

---
+++ stocktake/01_b2s_db3_to_db.py
@@ -0,0 +1,67 @@
+import pandas as pd
+from sqlalchemy import create_engine,text
+import db_connect
+from tqdm import tqdm
+
+bu = 'b2s'
+chunksize = 1000
+
+db = create_engine(db_connect.db_url_pstdb)
+db3 = create_engine(db_connect.db_url_pstdb3)
+
+def var_to_db3(bu, date_start, date_end):
+    print(f'Processing BU: {bu}, Date Range: {date_start} to {date_end}')
+    table = 'var'
+    # เตียมข้อมูลจาก db3
+    q_db3 = text(f"""
+    SELECT column_name, phycnt_cst, qty_count, phycnt_rtl, dept, sdept, class, sclass, sku, ibc,
+                 sbc, sku_desc, brndname, brnddesc, catalogue, color, size, retail, cost, count_user,
+                 cntdate, rpname, skutype, stocktakeid, bu, stcode, username, countname, store, batch
+    FROM {bu}_{table}_this_year
+    where cntdate between '{date_start}' and '{date_end}'
+    """)
+    df_db3 = pd.read_sql(q_db3, db3)
+    
+    # เตียมข้อมูลจาก db
+    q_db = f"""
+    SELECT distinct bu,stcode,cntdate,skutype,rpname
+    FROM {bu}_{table}
+    WHERE cntdate between '{date_start}' and '{date_end}'
+    """
+    df_db = pd.read_sql(q_db, db)
+
+    # Faster anti-join
+    keys = ['bu', 'stcode', 'cntdate', 'skutype', 'rpname']
+    mask = ~df_db3.set_index(keys).index.isin(df_db.set_index(keys).index)
+    df = df_db3[mask].reset_index(drop=True)
+
+    # ===== Insert with tqdm =====
+    total = len(df)
+
+    with tqdm(total=total, desc='🚀 Insert VAR', unit='rows') as pbar:
+        for start in range(0, total, chunksize):
+            end = start + chunksize
+            df.iloc[start:end].to_sql(
+                f'{bu}_{table}',
+                db,
+                if_exists='append',
+                index=False,
+                method='multi'   # เร็วขึ้นมาก
+            )
+            pbar.update(end - start)
+
+    print('✅ Insert completed')
+
+    
+var_to_db3(bu, '20250101', '20250131')
+var_to_db3(bu, '20250201', '20250231')
+var_to_db3(bu, '20250301', '20250331')
+var_to_db3(bu, '20250401', '20250431')
+var_to_db3(bu, '20250501', '20250531')
+var_to_db3(bu, '20250601', '20250631')
+var_to_db3(bu, '20250701', '20250731')
+var_to_db3(bu, '20250801', '20250831')
+var_to_db3(bu, '20250901', '20250931')
+var_to_db3(bu, '20251001', '20251031')
+var_to_db3(bu, '20251101', '20251131')
+var_to_db3(bu, '20251201', '20251231')

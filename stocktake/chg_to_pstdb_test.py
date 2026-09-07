8df21c8a6de948c384f888f1f147573ba3a09081
prthanapat <thanapatpromchat@gmail.com>
2026-09-07 18:26:27 +0700

    Update date range in chg_to_pstdb_test.py for data processing; enhance logging for record deletion and adjust print statements for clarity.

--- stocktake/chg_to_pstdb_test.py
+++ stocktake/chg_to_pstdb_test.py
@@ -11,13 +11,11 @@ engine1 = f"{os.getenv('DB_CONN_NATIVE')}{os.getenv('DB_USER')}:{os.getenv('DB_P
 engine2 = f"{os.getenv('DB_CONN_NATIVE')}{os.getenv('DB_USER')}:{os.getenv('DB_PASS')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_pstdb2')}"
 engine3 = f"{os.getenv('DB_CONN_NATIVE')}{os.getenv('DB_USER')}:{os.getenv('DB_PASS')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_pstdb3')}"
 
-bu = "chg"
-
 date = (datetime.now() - timedelta(days=7)).strftime("%Y%m%d")
 
-sdate = '20260401'
-edate = '20260431'
-print(f"Processing data for date: {date}")
+sdate = '20260601'
+edate = '20260631'
+print(f"Processing data for date: {sdate} to {edate}")
 
 
 print(f"Start Process: {datetime.now()}")
@@ -50,27 +48,27 @@ print(f"Total rows in chg_stk_this_year: {total_rows_stk}")
 # ---------------------------------------------------------------------
 try:
     if total_rows_var > 0:
+        print(f"Writing {total_rows_var} records to DB1 (chg_var)")
         df_var.write_database(
             table_name="chg_var",
             connection=engine1,
             if_table_exists="append",
             engine="adbc",
         )
-        print(f"Written {total_rows_var} records to DB1 (chg_var)")
+        
 
         # ---------------------------------------------------------------------
         # 3. เมื่อเขียนลง engine1 สำเร็จค่อยลบข้อมูลออกจาก engine3 (ต้นทาง)
         # ---------------------------------------------------------------------
         with adbc_psycopg.connect(engine3) as conn:
             with conn.cursor() as cursor:
+                print(f"Deleting records from DB3 (chg_var_this_year) for cntdate BETWEEN '{sdate}' AND '{edate}'")
                 cursor.execute(f"DELETE FROM chg_var_this_year WHERE cntdate BETWEEN '{sdate}' AND '{edate}'")
                 conn.commit()
-                #conn.autocommit = True
-                #cursor.execute("vacuum analyze chg_var_this_year")
-                #conn.autocommit = False
-        print(f"Deleted records from DB3 (chg_var_this_year) for cntdate = '{date}'")
+
+
     else:
-        print(f"No records found in DB3 (chg_var_this_year) for cntdate = '{date}'")
+        print(f"No records found in DB3 (chg_var_this_year) for cntdate BETWEEN '{sdate}' AND '{edate}'")
 
 except Exception as e:
     print(f"An error occurred: {e}")
@@ -83,27 +81,25 @@ print(f"End Process Var: {datetime.now()}")
 
 try:
     if total_rows_stk > 0:
+        print(f"Writing {total_rows_stk} records to DB1 (chg_stk)")
         df_stk.write_database(
             table_name="chg_stk",
             connection=engine1,
             if_table_exists="append",
             engine="adbc",
         )
-        print(f"Written {total_rows_stk} records to DB1 (chg_stk)")
+
 
         # ---------------------------------------------------------------------
         # 3. เมื่อเขียนลง engine1 สำเร็จค่อยลบข้อมูลออกจาก engine3 (ต้นทาง)
         # ---------------------------------------------------------------------
         with adbc_psycopg.connect(engine3) as conn:
             with conn.cursor() as cursor:
+                print(f"Deleting records from DB3 (chg_stk_this_year) for cntdate BETWEEN '{sdate}' AND '{edate}'")
                 cursor.execute(f"DELETE FROM chg_stk_this_year WHERE cntdate BETWEEN '{sdate}' AND '{edate}'")
                 conn.commit()
-                #conn.autocommit = True
-                #cursor.execute("vacuum analyze chg_stk_this_year")
-                #conn.autocommit = False
-        print(f"Deleted records from DB3 (chg_stk_this_year) for cntdate = '{date}'")
     else:
-        print(f"No records found in DB3 (chg_stk_this_year) for cntdate = '{date}'")
+        print(f"No records found in DB3 (chg_stk_this_year) for cntdate BETWEEN '{sdate}' AND '{edate}'")
 
 except Exception as e:
     print(f"An error occurred: {e}")

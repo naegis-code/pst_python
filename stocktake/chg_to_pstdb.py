bc04b823cd2a77eafaa31519ea19a146bd531476
prthanapat <thanapatpromchat@gmail.com>
2026-09-07 17:11:43 +0700

    Update date range in chg_to_pstdb.py for data processing; modify SQL queries to use BETWEEN for cntdate and enhance logging for written records.

--- stocktake/chg_to_pstdb.py
+++ stocktake/chg_to_pstdb.py
@@ -12,7 +12,11 @@ engine2 = f"{os.getenv('DB_CONN_NATIVE')}{os.getenv('DB_USER')}:{os.getenv('DB_P
 engine3 = f"{os.getenv('DB_CONN_NATIVE')}{os.getenv('DB_USER')}:{os.getenv('DB_PASS')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_pstdb3')}"
 
 bu = "chg"
+
 date = (datetime.now() - timedelta(days=7)).strftime("%Y%m%d")
+
+sdate = '20260401'
+edate = '20260431'
 print(f"Processing data for date: {date}")
 
 
@@ -24,44 +28,46 @@ print(f"Start Process: {datetime.now()}")
 var_query = f"""
     SELECT *
     FROM chg_var_this_year
-    WHERE cntdate <= '{date}'
+    WHERE cntdate BETWEEN '{sdate}' AND '{edate}'
 """
 
 stk_query = f"""
     SELECT *
     FROM chg_stk_this_year
-    WHERE cntdate <= '{date}'
+    WHERE cntdate BETWEEN '{sdate}' AND '{edate}'
 """
-
-df_stk = pl.read_database_uri(stk_query, uri=engine3)
-total_rows_stk = len(df_stk)
 
 df_var = pl.read_database_uri(var_query, uri=engine3)
 total_rows_var = len(df_var)
+print(f"Total rows in chg_var_this_year: {total_rows_var}")
+
+df_stk = pl.read_database_uri(stk_query, uri=engine3)
+total_rows_stk = len(df_stk)
+print(f"Total rows in chg_stk_this_year: {total_rows_stk}")
 
 # ---------------------------------------------------------------------
 # 2. บันทึกข้อมูลลง engine1 (ปลายทาง)
 # ---------------------------------------------------------------------
 try:
     if total_rows_var > 0:
-        print(f"Found {total_rows_var} records")
         df_var.write_database(
             table_name="chg_var",
             connection=engine1,
             if_table_exists="append",
             engine="adbc",
         )
+        print(f"Written {total_rows_var} records to DB1 (chg_var)")
 
         # ---------------------------------------------------------------------
         # 3. เมื่อเขียนลง engine1 สำเร็จค่อยลบข้อมูลออกจาก engine3 (ต้นทาง)
         # ---------------------------------------------------------------------
         with adbc_psycopg.connect(engine3) as conn:
             with conn.cursor() as cursor:
-                cursor.execute(f"DELETE FROM chg_var_this_year WHERE cntdate <= '{date}'")
+                cursor.execute(f"DELETE FROM chg_var_this_year WHERE cntdate BETWEEN '{sdate}' AND '{edate}'")
                 conn.commit()
-                conn.autocommit = True
-                cursor.execute("vacuum analyze chg_var_this_year")
-                conn.autocommit = False
+                #conn.autocommit = True
+                #cursor.execute("vacuum analyze chg_var_this_year")
+                #conn.autocommit = False
         print(f"Deleted records from DB3 (chg_var_this_year) for cntdate = '{date}'")
     else:
         print(f"No records found in DB3 (chg_var_this_year) for cntdate = '{date}'")
@@ -77,24 +83,24 @@ print(f"End Process Var: {datetime.now()}")
 
 try:
     if total_rows_stk > 0:
-        print(f"Found {total_rows_stk} records")
         df_stk.write_database(
             table_name="chg_stk",
             connection=engine1,
             if_table_exists="append",
             engine="adbc",
         )
+        print(f"Written {total_rows_stk} records to DB1 (chg_stk)")
 
         # ---------------------------------------------------------------------
         # 3. เมื่อเขียนลง engine1 สำเร็จค่อยลบข้อมูลออกจาก engine3 (ต้นทาง)
         # ---------------------------------------------------------------------
         with adbc_psycopg.connect(engine3) as conn:
             with conn.cursor() as cursor:
-                cursor.execute(f"DELETE FROM chg_stk_this_year WHERE cntdate <= '{date}'")
+                cursor.execute(f"DELETE FROM chg_stk_this_year WHERE cntdate BETWEEN '{sdate}' AND '{edate}'")
                 conn.commit()
-                conn.autocommit = True
-                cursor.execute("vacuum analyze chg_stk_this_year")
-                conn.autocommit = False
+                #conn.autocommit = True
+                #cursor.execute("vacuum analyze chg_stk_this_year")
+                #conn.autocommit = False
         print(f"Deleted records from DB3 (chg_stk_this_year) for cntdate = '{date}'")
     else:
         print(f"No records found in DB3 (chg_stk_this_year) for cntdate = '{date}'")
